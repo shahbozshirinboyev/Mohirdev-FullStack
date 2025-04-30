bed6c7578ea5b3311de37c400c8f8e2d2ca93c6c
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-04-30 16:54:23 +0500

    Son topish dasturi

--- python_asoslari/son_topish/functions.py
+++ python_asoslari/son_topish/functions.py
@@ -1,45 +1,51 @@
 import random
+import keyboard
 
 def son_top_user(x=10):
   son = random.randint(1, x)
   print(f"1 dan {x} gacha oraliqda son o'yladim: Topa olasizmi?")
-  sanoq = 0
+  urunishlar = 0
   while True:
     user_son = int(input(">> "))
+    urunishlar += 1
     if user_son == son:
       print(f"Tabriklaymiz, siz topdingiz! O'ylangan son: {son}")
-      sanoq += 1
       break
     elif son > user_son:
       print(f"Xato, men o'ylagan son bundan kattaroq. Yana harakat qiling:")
-      sanoq += 1
     else:
       print(f"Xato, men o'ylagan son bundan kichikroq. Yana harakat qiling:")
-      sanoq += 1
-  print(f"Tabriklaymiz, siz {sanoq} urunishda topdingiz!")
-  return sanoq
+  print(f"Tabriklaymiz, siz {urunishlar} urunishda topdingiz!")
+  return urunishlar
 
 def son_top_pc(x=10):
-    input(f"1 dan {x} gacha son o'ylang va istalgan tugmani bosing. Men topaman.")
-    quyi = 1
-    yuqori = x
-    taxminlar = 0
+  print(f"1 dan {x} gacha oraliqda son o'ylang. Men topishga harakat qilaman!")
+  print(f"Sonni o'ylagan bo'lsangiz istalgan tugmani bosing.")
+  keyboard.read_key(suppress=True)
+
+  past = 1
+  yuqori = x
+  urunishlar = 0
+
+  while True:
+    son = random.randint(past, yuqori)
+    urunishlar += 1
+    print(f"Siz o'ylagan son {son}\nAgar:\n(t) - to'g'ri bo'lsa\n(+) - o'ylagan son bu sondan katta\n(-) - o'ylagan son bu sondan kichik")
+
     while True:
-        taxminlar += 1
-        if quyi != yuqori:
-            taxmin = random.randint(quyi,yuqori)
-        else:
-            taxmin = quyi
-        javob = input(f"Siz {taxmin} sonini o'yladingiz: to'g'ri (t),"
-                      f"men o'ylagan son bundan kattaroq (+), yoki kichikroq (-)".lower())
-        if javob == "-":
-            yuqori = taxmin - 1
-        elif javob == "+":
-            quyi = taxmin + 1
-        else:
+        event = keyboard.read_event(suppress=True)
+        if event.event_type == 'down':
+            javob = event.name
             break
-    print(f"Men {taxminlar} ta taxmin bilan topdim!")
-    return taxminlar
+
+    if javob == 't':
+        print(f"Topdim! Siz {son} sonini o‘ylagansiz. Urunishlar soni: {urunishlar}")
+        break
+    elif javob == '+':
+        past = son + 1
+    elif javob == '-':
+        yuqori = son - 1
+    return urunishlar
 
 def play(x=10):
     yana = True
@@ -47,10 +53,12 @@ def play(x=10):
         taxminlar_user = son_top_user(x)
         taxminlar_pc = son_top_pc(x)
 
-        if taxminlar_user>taxminlar_pc:
+        if taxminlar_user > taxminlar_pc:
             print(f"Men {taxminlar_pc} taxmin bilan topdim va  yutdim!")
-        elif taxminlar_user<taxminlar_pc:
+        elif taxminlar_user < taxminlar_pc:
             print(f"Siz {taxminlar_user} taxmin bilan topdingiz va yutdingiz!")
         else:
             print("Durrang!")
         yana = int(input("Yana o'ynaymizmi? Ha(1)/Yo'q(0):"))
+
+play()
