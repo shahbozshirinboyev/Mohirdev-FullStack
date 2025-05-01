3cebfbd3ccd175c1c037e199d4bf88057ac89b4e
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-05-01 10:50:54 +0500

    lotin rill translate bot

--- python_asoslari/lotin_krill_bot/main.py
+++ python_asoslari/lotin_krill_bot/main.py
@@ -19,11 +19,12 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
   msg = message.text
-  if msg.isascii():
-    javob = to_cyrillic(msg)
-  else:
-    javob = to_latin(msg)
-  bot.reply_to(message, javob)
+  javob = lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg)
+  # if msg.isascii():
+  #   javob = to_cyrillic(msg)
+  # else:
+  #   javob = to_latin(msg)
+  bot.reply_to(message, javob(msg))
 
 bot.infinity_polling()
 
