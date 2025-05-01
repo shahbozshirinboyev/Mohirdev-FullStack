47fb8684a54a02596ba1b60b3c8d9fb5c599123f
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-05-01 10:23:03 +0500

    Telegram bot connect and works function

--- python_asoslari/lotin_krill_bot/main.py
+++ python_asoslari/lotin_krill_bot/main.py
@@ -5,9 +5,32 @@ Lotin - Krill BOT
 Dasturchi: Shahboz Shirinboyev
 """
 from transliterate import to_cyrillic, to_latin
+import telebot
 
-matn = input("Matn kiriting: ")
-if matn.isascii():
-  print(to_cyrillic(matn))
-else:
-  print(to_latin(matn))
+TOKEN = '7144085404:AAHw_hG_Pfq8WQGDtsZARu65lmIOUthYgUo'
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+  javob = "Assalomu alaykum, Xush kelibsiz!"
+  javob += "\nMatn kiriting:"
+  bot.reply_to(message, javob)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+  msg = message.text
+  if msg.isascii():
+    javob = to_cyrillic(msg)
+  else:
+    javob = to_latin(msg)
+  bot.reply_to(message, javob)
+
+bot.infinity_polling()
+
+
+
+# matn = input("Matn kiriting: ")
+# if matn.isascii():
+#   print(to_cyrillic(matn))
+# else:
+#   print(to_latin(matn))
