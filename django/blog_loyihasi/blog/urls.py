3be74b9ceaa8acc075cd2bddbf5511f5901bf7fb
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-06-03 18:11:12 +0500

    funksiyalarga asoslangan view.py

--- django/blog_loyihasi/blog/urls.py
+++ django/blog_loyihasi/blog/urls.py
@@ -1,6 +1,7 @@
-from .views import bloglistview
+from .views import bloglistview, blogdetailview
 from django.urls import path
 
 urlpatterns = [
   path("", bloglistview, name="blog_list_view"),
+  path("blogs/<int:id>/", blogdetailview, name="blog_detail"),
 ]
--- django/blog_loyihasi/blog/views.py
+++ django/blog_loyihasi/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -10,3 +10,20 @@ def bloglistview(request):
   }
 
   return render(request, "home.html", context=context)
+
+def blogdetailview(request, id):
+  blog = get_object_or_404(Blog, id=id)
+  context = {
+    'blog': blog
+  }
+
+  # try:
+  #   blog = Blog.objects.get(id=id)
+  #   context = {
+  #     "blog": blog,
+  #   }
+  # except Blog.DoesNotExist:
+  #   raise Http404("No blog found")
+
+  return render(request, "blog_detail.html", context=context)
+# 10.37 sec
