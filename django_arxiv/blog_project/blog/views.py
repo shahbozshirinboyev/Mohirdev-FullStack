71757febd8cddac700ca818ee58aa576e997f971
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-06-19 19:12:00 +0500

    Blog project 2-qism

--- django_arxiv/blog_project/blog/views.py
+++ django_arxiv/blog_project/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 class BlogListView(ListView):
@@ -10,3 +11,8 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
   model = Post
   template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+  model = Post
+  template_name = 'post_new.html'
+  fields = ['title', 'author', 'body']
