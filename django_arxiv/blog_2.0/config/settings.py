c09ad89648161db94fb8772f2771fa073c343075
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-06-22 07:19:59 +0500

    new code ...

--- django_arxiv/blog_2.0/config/settings.py
+++ django_arxiv/blog_2.0/config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # installed apps
+    'crispy_forms',
+    'crispy_bootstrap5',
+    # created apps
     'accounts',
     'pages',
 ]
@@ -127,3 +131,6 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
