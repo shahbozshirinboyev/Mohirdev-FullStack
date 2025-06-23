f3dba54e9ef92097e4a47c3fc06fd8b64259dbba
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-06-23 15:44:31 +0500

    Izoh qoldirishni qoldik

--- django_arxiv/blog_2.0/articles/admin.py
+++ django_arxiv/blog_2.0/articles/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
 # Register your models here.
-admin.site.register(Article)
+class CommentInline(admin.TabularInline):
+  model = Comment
+  extra = 0 #qo'shimcha bo'sh joy uchun
+
+class ArticleAdmin(admin.ModelAdmin):
+  inlines = [CommentInline]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- django_arxiv/blog_2.0/articles/models.py
+++ django_arxiv/blog_2.0/articles/models.py
@@ -20,3 +20,15 @@ class Article(models.Model):
 
   def get_absolute_url(self):
     return reverse('article_detail', args=[str(self.id)])
+
+class Comment(models.Model):
+  article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments',)
+  commet = models.CharField(max_length=150)
+  created_at = models.DateTimeField(auto_now_add=True)
+  author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
+
+  def __str__(self):
+    return self.commet
+
+  def get_absolute_url(self):
+    return reverse('article_list')
