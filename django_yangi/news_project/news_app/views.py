79751d0f925525671b107b25dd39b930021e1e9f
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-06-27 17:29:06 +0500

    4.42 - sekundda qoldi

--- django_yangi/news_project/news_app/views.py
+++ django_yangi/news_project/news_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Category, News
 
 # Create your views here.
+def news_list(request):
+  news_list = News.objects.all()
+  context = {
+    'new_list': news_list
+  }
+  return render(request, 'news/news_list.html', context)
