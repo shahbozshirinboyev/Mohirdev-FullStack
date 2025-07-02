d8c1190db15cc641a5f45543306284481e5e03b9
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-07-02 19:27:28 +0500

    the lastes news ...

--- django_yangi/news_project/news_app/views.py
+++ django_yangi/news_project/news_app/views.py
@@ -22,10 +22,10 @@ def news_detail(request, id):
   return render(request, 'news/news_detail_page.html', context)
 
 def homePageView(request):
-  news = News.published.all()
+  news_list = News.published.all().order_by('-publish_time')
   categories = Category.objects.all()
   context = {
-    'news': news,
+    'news_list': news_list,
     'categories': categories
   }
   return render(request, 'news/index.html', context)
