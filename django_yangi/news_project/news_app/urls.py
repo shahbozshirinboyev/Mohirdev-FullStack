d08e60da38446fa1075f15fa226c1dbada1f32fb
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-07-02 17:52:09 +0500

    view function to view class

--- django_yangi/news_project/news_app/urls.py
+++ django_yangi/news_project/news_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import news_list, news_detail, homePageView, contactPageView, categoryPageView
+from .views import news_list, news_detail, homePageView, ContactPageView, categoryPageView
 
 urlpatterns = [
   path('', homePageView, name='home_page'),
-  path('contact/', contactPageView, name='contact'),
+  path('contact/', ContactPageView.as_view(), name='contact'),
   path('catagory/', categoryPageView, name='catagory'),
   path('news/', news_list, name='all_news_list'),
   path('news/<int:id>/', news_detail, name='news_detail_page')
--- django_yangi/news_project/news_app/views.py
+++ django_yangi/news_project/news_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, News
 from .forms import ContactForm
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 def news_list(request):
@@ -29,15 +30,36 @@ def homePageView(request):
   }
   return render(request, 'news/index.html', context)
 
-def contactPageView(request):
-    form = ContactForm(request.POST or None)
-    if request.method == 'POST' and form.is_valid():
-       form.save()
-       return HttpResponse("<h2>Biz bilan bog'langaningiz uchun rahmat!</h2>")
-    context = {
-       'form': form
-    }
-    return render(request, 'news/contact.html', context)
-
 def categoryPageView(request):
     return render(request, 'news/catagory.html')
+
+# def contactPageView(request):
+#     form = ContactForm(request.POST or None)
+#     if request.method == 'POST' and form.is_valid():
+#        form.save()
+#        return HttpResponse("<h2>Biz bilan bog'langaningiz uchun rahmat!</h2>")
+#     context = {
+#        'form': form
+#     }
+#     return render(request, 'news/contact.html', context)
+
+# contactPageView'ni class orqali ham qilib ko'ramiz:
+class ContactPageView(TemplateView):
+   template_name = 'news/contact.html'
+
+   def get(self, request, *args, **kwargs):
+      form = ContactForm
+      context = {
+         'form': form,
+      }
+      return render(request, 'news/contact.html', context)
+
+   def post(self, request, *args, **kwargs):
+      form = ContactForm(request.POST)
+      if request.method == 'POST' and form.is_valid():
+        form.save()
+        return HttpResponse("<h2>Biz bilan bog'langaningiz uchun rahmat!</h2>")
+      context = {
+         'form': form,
+      }
+      return render(request, 'news/contact.html', context)
