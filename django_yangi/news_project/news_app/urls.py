1c4a5fd8c71282f2791103aca63f722d2b7d5639
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-07-03 11:26:16 +0500

    context processor bilan ishlash

--- django_yangi/news_project/news_app/urls.py
+++ django_yangi/news_project/news_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import news_list, news_detail, homePageView, ContactPageView, categoryPageView
+from .views import news_list, news_detail, HomePageView, ContactPageView, categoryPageView, AboutPageView
 
 urlpatterns = [
-  path('', homePageView, name='home_page'),
-  path('contact/', ContactPageView.as_view(), name='contact'),
+  path('', HomePageView.as_view(), name='home_page'),
   path('catagory/', categoryPageView, name='catagory'),
+  path('about/', AboutPageView, name='about'),
+  path('contact/', ContactPageView.as_view(), name='contact'),
   path('news/', news_list, name='all_news_list'),
   path('news/<int:id>/', news_detail, name='news_detail_page')
 ]
--- django_yangi/news_project/news_app/views.py
+++ django_yangi/news_project/news_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, News
 from .forms import ContactForm
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 # Create your views here.
 def news_list(request):
@@ -21,21 +21,37 @@ def news_detail(request, id):
   }
   return render(request, 'news/news_detail_page.html', context)
 
-def homePageView(request):
-  news_list = News.published.all().order_by('-publish_time')
-  categories = Category.objects.all()
-  sport_news_one = News.published.all().filter(category__name='Sport').order_by('-publish_time')[0]
-  sport_news_two = News.published.filter(category__name='Sport').order_by('-publish_time')[1:3]
-  context = {
-    'news_list': news_list,
-    'categories': categories,
-    'sport_news_one': sport_news_one,
-    'sport_news_two': sport_news_two,
-  }
-  return render(request, 'news/index.html', context)
+# def homePageView(request):
+#   news_list = News.published.all().order_by('-publish_time')
+#   categories = Category.objects.all()
+#   sport_news_one = News.published.all().filter(category__name='Sport').order_by('-publish_time')[0]
+#   sport_news_two = News.published.filter(category__name='Sport').order_by('-publish_time')[1:3]
+#   context = {
+#     'news_list': news_list,
+#     'categories': categories,
+#     'sport_news_one': sport_news_one,
+#     'sport_news_two': sport_news_two,
+#   }
+#   return render(request, 'news/index.html', context)
+
+class HomePageView(ListView):
+   model = News
+   template_name = 'news/index.html'
+   context_object_name = 'news'
+
+   def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      context['categories'] = Category.objects.all()
+      context['news_list'] = News.published.all().order_by('-publish_time')
+      context['sport_news_one'] = News.published.all().filter(category__name='Sport').order_by('-publish_time')[0]
+      context['sport_news_two'] = News.published.filter(category__name='Sport').order_by('-publish_time')[1:3]
+      return context
 
 def categoryPageView(request):
     return render(request, 'news/catagory.html')
+
+def AboutPageView(request):
+    return render(request, 'news/about.html')
 
 # def contactPageView(request):
 #     form = ContactForm(request.POST or None)
@@ -48,6 +64,7 @@ def categoryPageView(request):
 #     return render(request, 'news/contact.html', context)
 
 # contactPageView'ni class orqali ham qilib ko'ramiz:
+
 class ContactPageView(TemplateView):
    template_name = 'news/contact.html'
 
