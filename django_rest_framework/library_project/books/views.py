23d80509bb88ae2aefec1416478035ff3af327e5
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-07-24 18:53:47 +0500

    generic view'lar bilan ishlash

--- django_rest_framework/library_project/books/views.py
+++ django_rest_framework/library_project/books/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+class BookListApiView(generics.ListAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+
+class BookDetailApiView(generics.RetrieveAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+
+class BookDeleteApiView(generics.DestroyAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+
+class BookUpdateApiView(generics.UpdateAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+
+class BookCreateApiView(generics.CreateAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+
+class BookListCreateApiView(generics.ListCreateAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+
+class BookListUpdateDeleteApiView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+
+# Function based viewin DRF
+@api_view(['GET'])
+def book_list_view(request, *args, **kwargs):
+  books = Book.objects.all()
+  serializer = BookSerializer(books, many=True)
+  return Response(serializer.data)
--- django_rest_framework/library_project/config/settings.py
+++ django_rest_framework/library_project/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # packages
+    'rest_framework',
+    # local apps
     'books',
 ]
 
