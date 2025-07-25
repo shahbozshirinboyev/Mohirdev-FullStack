2fa9e3bf6683252b4fbb6f94ee8223cc41b84c89
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-07-25 14:54:33 +0500

    ModelSerializer va Serializer

--- django_rest_framework/library_project/books/serializers.py
+++ django_rest_framework/library_project/books/serializers.py
@@ -3,6 +3,42 @@ from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
 
+  title = serializers.CharField(max_length=150)
+
   class Meta:
     model = Book
     fields = ('id', 'title', 'subtitle', 'content', 'author', 'isbn', 'price',)
+
+# class BookSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(max_length=200)
+#     subtitle = serializers.CharField(max_length=200)
+#     content = serializers.CharField()
+#     author = serializers.CharField(max_length=100)
+#     isbn = serializers.CharField(max_length=13)
+#     price = serializers.DecimalField(max_digits=20, decimal_places=2)
+
+#     def create(self, validated_data):
+#         return Book.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.subtitle = validated_data.get('subtitle', instance.subtitle)
+#         instance.content = validated_data.get('content', instance.content)
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.isbn = validated_data.get('isbn', instance.isbn)
+#         instance.price = validated_data.get('price', instance.price)
+#         instance.save()
+#         return instance
+
+#     def validate_price(self, value):
+#         if value <= 0:
+#             raise serializers.ValidationError("Narx musbat bo‘lishi kerak.")
+#         return value
+
+#     def validate_isbn(self, value):
+#         if not value.isdigit():
+#             raise serializers.ValidationError("ISBN faqat raqamlardan iborat bo‘lishi kerak.")
+#         if len(value) != 13:
+#             raise serializers.ValidationError("ISBN uzunligi 13 ta belgidan iborat bo‘lishi kerak.")
+#         return value
--- django_rest_framework/library_project/books/views.py
+++ django_rest_framework/library_project/books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics, status
+from rest_framework.generics import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -44,13 +45,44 @@ class BookDetailApiView(APIView):
         status=status.HTTP_404_NOT_FOUND
       )
 
-class BookDeleteApiView(generics.DestroyAPIView):
-  queryset = Book.objects.all()
-  serializer_class = BookSerializer
+# class BookDeleteApiView(generics.DestroyAPIView):
+#   queryset = Book.objects.all()
+#   serializer_class = BookSerializer
+class BookDeleteApiView(APIView):
+  def delete(self, request, pk):
+    try:
+      book = Book.objects.get(id=pk)
+      book.delete()
+      return Response(
+        {
+          "status": True,
+          "message": "Book is delete."
+        }, status=status.HTTP_200_OK
+      )
+    except:
+      return Response(
+        {
+          "status": False,
+          "message": "Book is not found."
+        }, status=status.HTTP_400_BAD_REQUEST
+      )
 
-class BookUpdateApiView(generics.UpdateAPIView):
-  queryset = Book.objects.all()
-  serializer_class = BookSerializer
+# class BookUpdateApiView(generics.UpdateAPIView):
+#   queryset = Book.objects.all()
+#   serializer_class = BookSerializer
+class BookUpdateApiView(APIView):
+  def put(self, request, pk):
+    book = get_object_or_404(Book.objects.all(), id=pk)
+    data = request.data
+    serializer = BookSerializer(instance=book, data=data, partial=True)
+    if serializer.is_valid(raise_exception=True):
+      book_saved = serializer.save()
+    return Response(
+      {
+        "status": True,
+        "message": f"Book {book_saved} updated successfully."
+      }
+    )
 
 # class BookCreateApiView(generics.CreateAPIView):
 #   queryset = Book.objects.all()
