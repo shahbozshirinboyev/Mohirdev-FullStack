bbd46afeead5289e1b12eb0a8639102a878ae0f1
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-07-28 20:16:17 +0500

    UserConfirmation model

--- django_rest_framework/instagram_clone/config/settings.py
+++ django_rest_framework/instagram_clone/config/settings.py
@@ -41,6 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # packages
+    'rest_framework',
+
+    # apps
+    'users',
+    'shared',
 ]
 
 MIDDLEWARE = [
@@ -123,8 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
