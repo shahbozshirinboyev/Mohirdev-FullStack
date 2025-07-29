1bcedc5ccdb04f09b774c6b3fc907f2498a75f65
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-07-29 15:55:07 +0500

    create some functions

--- django_rest_framework/instagram_clone/users/views.py
+++ django_rest_framework/instagram_clone/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import User
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView
+from rest_framework.decorators import permission_classes
+from .serializers import SignUpSerializer
+
 
 # Create your views here.
+class CreateUserView(CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny, )
+    serializer_class = SignUpSerializer
