6781dd6b6fd8429022fb1a176a3d60e6e0da2e44
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-08-03 16:37:59 +0500

    Python&Django - goodreads - add models

---
+++ 11. Python&Django/goodreads/books/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Book(models.Model):
+  title = models.CharField(max_length=200)
+  description = models.TextField()
+  isbn = models.CharField(max_length=17)
+
+class Author(models.Model):
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  email = models.EmailField()
+  bio = models.TextField()
+
+class BookAuthor(models.Model):
+  book = models.ForeignKey(Book, on_delete=models.CASCADE)
+  author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+class BookReview(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  book = models.ForeignKey(Book, on_delete=models.CASCADE)
+  comment = models.TextField()
+  stars_given = models.IntegerField(
+    validators=[MinValueValidator(0), MaxValueValidator(5)]
+  )
