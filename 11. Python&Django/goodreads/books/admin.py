32e80318e38441ba2096b134765d0087cc06d6f5
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-08-03 17:39:59 +0500

    Python&Django - goodreads - Introducion Admin Panel

--- 11. Python&Django/goodreads/books/admin.py
+++ 11. Python&Django/goodreads/books/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from books.models import Book, Author, BookAuthor, BookReview
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+  search_fields = ('title', 'isbn')
+  list_display = ('title', 'isbn', 'description')
+
+class AuthorAdmin(admin.ModelAdmin):
+  search_fields = ('first_name', 'last_name')
+  list_display = ('first_name', 'last_name', 'email', 'bio')
+
+class BookAuthorAdmin(admin.ModelAdmin):
+  search_fields = ('book', 'author')
+  list_display = ('book', 'author')
+
+class BookReviewAdmin(admin.ModelAdmin):
+  search_fields = ('user', 'book', 'stars_given')
+  list_display = ('user', 'book', 'stars_given', 'comment')
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(BookAuthor, BookAuthorAdmin)
+admin.site.register(BookReview, BookReviewAdmin)
--- 11. Python&Django/goodreads/books/models.py
+++ 11. Python&Django/goodreads/books/models.py
@@ -7,15 +7,25 @@ class Book(models.Model):
   description = models.TextField()
   isbn = models.CharField(max_length=17)
 
+  def __str__(self):
+    return self.title
+
 class Author(models.Model):
   first_name = models.CharField(max_length=100)
   last_name = models.CharField(max_length=100)
   email = models.EmailField()
   bio = models.TextField()
 
+  def __str__(self):
+    full_name = f"{self.first_name} {self.last_name}"
+    return full_name
+
 class BookAuthor(models.Model):
   book = models.ForeignKey(Book, on_delete=models.CASCADE)
   author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.book.title} by {self.author.first_name} {self.author.last_name}"
 
 class BookReview(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -24,3 +34,6 @@ class BookReview(models.Model):
   stars_given = models.IntegerField(
     validators=[MinValueValidator(0), MaxValueValidator(5)]
   )
+
+  def __str__(self):
+    return f"{self.stars_given} stars for {self.book.title} by {self.user.username}"
