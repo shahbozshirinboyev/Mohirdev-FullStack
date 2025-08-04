fd851bfd652d610ae2a028566ac847c5f6d57db0
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-08-04 07:56:43 +0500

    Python&Django - goodreads - forms section

---
+++ 11. Python&Django/goodreads/users/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class UserCreateForm(forms.Form):
+  pass
--- 11. Python&Django/goodreads/users/views.py
+++ 11. Python&Django/goodreads/users/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.models import User
 
 class RegisterView(View):
   def get(self, request):
     return render(request, 'users/register.html')
+
+  def post(self, request):
+    username = request.POST['username']
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    email = request.POST['email']
+    password = request.POST['password']
+    user = User.objects.create( username=username, first_name=first_name, last_name=last_name, email=email, )
+    user.set_password(password)
+    user.save()
+    return redirect('users:login')
 
 class LoginView(View):
   def get(self, request):
