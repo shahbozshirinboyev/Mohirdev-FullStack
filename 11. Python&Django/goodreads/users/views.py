df94d573bcd35bb36b2a186bb5893ee7c1e1c8d6
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-08-05 09:34:41 +0500

    Logout/Messages/BaseTemplate - goodreads

--- 11. Python&Django/goodreads/users/views.py
+++ 11. Python&Django/goodreads/users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import AuthenticationForm
 from users.forms import UserCreateForm
+from django.contrib import messages
 
 class RegisterView(View):
   def get(self, request):
@@ -23,23 +24,28 @@ class RegisterView(View):
       context = { "form": create_form }
       return render(request, 'users/register.html', context)
 
-class LoginView(View):
+class ProfileView(LoginRequiredMixin, View):
+  def get(self, request):
+    context = { 'user': request.user }
+    return render(request, 'users/profile.html', context)
 
+class LoginView(View):
   def get(self, request):
     login_form = AuthenticationForm()
     return render(request, 'users/login.html', {"login_form": login_form})
 
   def post(self, request):
     login_form = AuthenticationForm(data=request.POST)
-
     if login_form.is_valid():
       user = login_form.get_user()
       login(request, user)
-      return redirect('index')
+      messages.success(request, 'You have successfully logged in.')
+      return redirect('books:list')
     else:
       return render(request, 'users/login.html', {"login_form": login_form})
 
-class ProfileView(LoginRequiredMixin, View):
+class LogoutView(LoginRequiredMixin, View):
   def get(self, request):
-    context = { 'user': request.user }
-    return render(request, 'users/profile.html', context)
+    logout(request)
+    messages.info(request, 'You have successfully logged out.')
+    return redirect('index')
