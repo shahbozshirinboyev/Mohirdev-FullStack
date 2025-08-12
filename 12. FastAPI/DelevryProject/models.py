7988b4d1ae56093378b83998d9540d74128d90ed
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-08-12 19:10:26 +0500

    Create models use Alschemas

---
+++ 12. FastAPI/DelevryProject/models.py
@@ -0,0 +1,45 @@
+from database import Base
+from sqlalchemy import Column, Integer, Boolean, Text, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils.types import ChoiceType
+
+class Users(Base):
+  __tablename__ = 'users'
+  id = Column(Integer, primary_key=True)
+  username = Column(String(25), unique=True)
+  email = Column(String(70), nullable=True)
+  password = Column(Text, nullable=True)
+  is_staff = Column(Boolean, default=False)
+  is_active = Column(Boolean, default=False)
+  orders = relationship('Orders', back_populates='users')
+
+  def __repr__(self):
+    return f"<user {self.username}"
+
+class Orders(Base):
+  ORDER_STATUSES = (
+    ('PENDING', 'pending'),
+    ('IN_TRANSIT', 'in_transit'),
+    ('DELIVERED', 'delivered'),
+
+  )
+  __tablename__ = 'orders'
+  id = Column(Integer, primary_key=True)
+  quantity = Column(Integer, nullable=False)
+  order_statuses = Column(ChoiceType(choices=ORDER_STATUSES), default='PENDING')
+  user_id = Column(Integer, ForeignKey('users.id'))
+  user = relationship('Users', back_populates='orders')
+  product_id = Column(Integer, ForeignKey('product.id'))
+  product = relationship('Product', back_populates='orders')
+
+  def __repr__(self):
+    return f"<order {self.id}"
+
+class Product(Base):
+  __tablename__ = 'product'
+  id = Column(Integer, primary_key=True)
+  name = Column(String(100))
+  price = Column(Integer)
+
+  def __repr__(self):
+    return f"<product {self.name}"
